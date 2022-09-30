f6519ac47d54148197c455ff079b11ea5844795c
killer1503999 <nk1880571@gmail.com>
2022-09-30 17:02:04 +0530

    deleted images

--- CRUD_app/Resources.py
+++ CRUD_app/Resources.py
@@ -1,5 +1,12 @@
 import os
 
+# key_vault = os.environ['key_vault']
+# vault_url = "https://"+key_vault+".vault.azure.net/"
+# AZURE_APP_BLOB_NAME = os.environ['AZURE_APP_BLOB_NAME']  # enc variable
+# AZURE_BLOB_PATH = os.environ['AZURE_BLOB_PATH']
+# DBName = os.environ['DBName']
+# DB_host = os.environ['DB_host']
+
 key_vault = "nileshkeyvault"
 vault_url = "https://"+key_vault+".vault.azure.net/"
 AZURE_APP_BLOB_NAME = "nileshimagescontainer"  # enc variable
