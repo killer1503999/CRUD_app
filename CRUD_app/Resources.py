b3832bcbbc15f6f2b3de2d86c3864ac6ee678e60
killer1503999 <nk1880571@gmail.com>
2022-10-07 16:01:17 +0530

    added env variables

--- CRUD_app/Resources.py
+++ CRUD_app/Resources.py
@@ -1,15 +1,15 @@
 import os
 
-# key_vault = os.environ['key_vault']
-# vault_url = "https://"+key_vault+".vault.azure.net/"
-# AZURE_APP_BLOB_NAME = os.environ['AZURE_APP_BLOB_NAME']  # enc variable
-# AZURE_BLOB_PATH = os.environ['AZURE_BLOB_PATH']
-# DBName = os.environ['DBName']
-# DB_host = os.environ['DB_host']
-
-key_vault = "nileshkeyvault"
+key_vault = os.getenv("key_vault")
 vault_url = "https://"+key_vault+".vault.azure.net/"
-AZURE_APP_BLOB_NAME = "nileshimagescontainer"  # enc variable
-AZURE_BLOB_PATH = "https://assessmentstgacc.blob.core.windows.net/nileshimagescontainer/nileshimagescontainer"
-DBName = "Nilesh_Crud_Database"
-DB_host = "assessmentserverget.database.windows.net"
+AZURE_APP_BLOB_NAME = os.getenv('AZURE_APP_BLOB_NAME')  # enc variable
+AZURE_BLOB_PATH = os.getenv('AZURE_BLOB_PATH')
+DBName = os.getenv('DBName')
+DB_host = os.getenv('DB_host')
+
+# key_vault = "nileshkeyvault"
+# vault_url = "https://"+key_vault+".vault.azure.net/"
+# AZURE_APP_BLOB_NAME = "nileshimagescontainer"  # enc variable
+# AZURE_BLOB_PATH = "https://assessmentstgacc.blob.core.windows.net/nileshimagescontainer/nileshimagescontainer"
+# DBName = "Nilesh_Crud_Database"
+# DB_host = "assessmentserverget.database.windows.net"
